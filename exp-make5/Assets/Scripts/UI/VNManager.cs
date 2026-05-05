892eb75623898e88803bcdfb5e2c5327be998d65
Wbdm <62707928+wbdm0001@users.noreply.github.com>
2026-05-05 22:24:21 +0900

    change

---
+++ exp-make5/Assets/Scripts/UI/VNManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VNManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator EventStarter()
+    {
+        yield return new WaitForSeconds(1);
+
+    }
+}
