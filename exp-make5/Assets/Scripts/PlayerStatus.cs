d6abea6d5e4addf8d2b8b6e8cebb7cdd890affa1
sanghyun003 <144121088+sanghyun003@users.noreply.github.com>
2026-05-05 23:42:25 +0900

    chore: 프로퍼티와 event를 통해 UI와 상호작용
    PlayerStatus의 체력, 부적 개수를 UI와 상호작용하기 위에 프로퍼티와 event를 추가

--- exp-make5/Assets/Scripts/PlayerStatus.cs
+++ exp-make5/Assets/Scripts/PlayerStatus.cs
@@ -1,19 +1,29 @@
+using System;
 using UnityEngine;
 
 // 이 스크립트는 오직 캐릭터의 '상태'만 관리합니다.
 public class PlayerStatus : MonoBehaviour
 {
-    [Header("Player Stats")]
-    public int maxHealth = 3;
-    public int currentHealth;
+    // 프로퍼티를 통해 체력, 빨간 부적, 파란 부적의 수가 변할 때마다 UI측으로 event를 통해 알려줌
+    public event Action<int> OnHealthChanged;
+    public event Action<int> OnRedAmuletChanged;
+    public event Action<int> OnBlueAmuletChanged;
+
+    [SerializeField] private int maxHealth = 3;
+    private int currentHealth;
     
-    [Header("Inventory")]
-    public int redAmuletCount = 1;
-    public int blueAmuletCount = 1;
+    private int redAmuletCount = 1;
+    private int blueAmuletCount = 1;
+
+    public int RedAmuletCount => redAmuletCount; 
+    public int BlueAmuletCount => blueAmuletCount;
+    public int CurrentHealth => currentHealth;
 
     void Start()
     {
         currentHealth = maxHealth;
+
+        OnHealthChanged?.Invoke(currentHealth);
     }
 
     // 빨간 몬스터를 만났을 때 맵 매니저가 호출할 함수
@@ -23,6 +33,9 @@ public class PlayerStatus : MonoBehaviour
         {
             redAmuletCount--;
             blueAmuletCount++;
+
+            OnRedAmuletChanged?.Invoke(redAmuletCount);
+            OnBlueAmuletChanged?.Invoke(blueAmuletCount);
             Debug.Log("빨간 몬스터 처치! 빨간 부적 소비 -> 파란 부적 획득");
         }
         else
@@ -38,6 +51,9 @@ public class PlayerStatus : MonoBehaviour
         {
             blueAmuletCount--;
             redAmuletCount++;
+
+            OnBlueAmuletChanged?.Invoke(blueAmuletCount);
+            OnRedAmuletChanged?.Invoke(redAmuletCount);
             Debug.Log("파란 몬스터 처치! 파란 부적 소비 -> 빨간 부적 획득");
         }
         else
@@ -49,6 +65,8 @@ public class PlayerStatus : MonoBehaviour
     private void TakeDamage()
     {
         currentHealth--;
+
+        OnHealthChanged?.Invoke(currentHealth);
         Debug.Log($"부적이 부족합니다! 체력 감소. 남은 체력: {currentHealth}");
         
         if (currentHealth <= 0)
